de36e06ffd1e893f1b2ef95d22ad82004718b0b8
Bianca H. Rangel <51705719+biancarangel@users.noreply.github.com>
2022-09-16 23:01:50 -0300

    Create Empresa.dart

---
+++ Empresa.dart
@@ -0,0 +1,17 @@
+import 'package:Socio/Socio.dart' as Socio;
+
+class Empresa {
+  int? id;
+  String? razaoSocial;
+  String? nomeFantasia;
+  String? cnpj;
+  String? logradouro;
+  int? numero;
+  String? complemento;
+  String? bairro;
+  String? estado;
+  String? cep;
+  String? telefone;
+  final? horarioCadastro;
+  Socio? socio;
+}
